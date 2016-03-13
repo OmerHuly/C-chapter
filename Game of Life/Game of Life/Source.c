0e9af558882e2fc64595e5555f74f593cf3a627c
OmerHuly <omer.huli@gmail.com>
2016-03-13 18:46:37 +0200

    Added files via upload

---
+++ Game of Life/Game of Life/Source.c
@@ -0,0 +1,21 @@
+/*
+GAME OF LIFE:
+Each cell can be dead or alive.
+Each game starts by drawing a pattern and then watch it as it evolves.
+A cell becomes alive if exactly 3 nearby cells are alive (birth).
+A cell stays alive if 2 or 3 nearby cells are alive (survival).
+In all other cases, a cell dies or remains dead (overcrowding or loneliness).
+
+The Rules:
+Each cell with 3 neighbors becomes populated.
+Each cell with 2 or 3 neighbors survives.
+Each cell with 1 or less neighbors dies, as if by solitude.
+Each cell with 4 or more neighbors dies, as if by overpopulation.
+*/
+
+#include <stdio.h>
+
+int main ()
+{
+	return 0;
+}
