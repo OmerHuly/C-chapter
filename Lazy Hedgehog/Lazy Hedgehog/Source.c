785469e2beb378be9f5ee043fe19805e0dee6560
OmerHuly <omer.huli@gmail.com>
2016-03-13 18:45:55 +0200

    Added files via upload

---
+++ Lazy Hedgehog/Lazy Hedgehog/Source.c
@@ -0,0 +1,14 @@
+/*
+The lazy hedgehog starts on top left corner of a 100X100 tiles board.
+Each tile has a value between 0 to 1000.
+He needs to reach the bottom right corner with the minimanl sum.
+The world matrix is in a file -> Needs to be read.
+The program prints out the easiest path (i.e. the minimal sum).
+*/
+
+#include <stdio.h>
+
+int main()
+{
+	return 0;
+}
